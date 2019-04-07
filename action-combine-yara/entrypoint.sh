e47a2cdcd6708c57bfa5d3935494dcd1f9ff1ffb
James Pleger <jpleger@gmail.com>
2019-04-06 22:45:14 -0700

    adding some more test stuff for actions

--- action-combine-yara/entrypoint.sh
+++ action-combine-yara/entrypoint.sh
@@ -4,3 +4,5 @@ set -eu
 # Combine all the yara files in the rules/ directory
 cat $GITHUB_WORKSPACE/rules/*.yara > $GITHUB_WORKSPACE/all.yara
 cat $GITHUB_WORKSPACE/all.yara
+cd $GITHUB_WORKSPACE
+ls -al
